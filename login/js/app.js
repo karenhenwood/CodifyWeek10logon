be52366556c085e94fb83390bdd8a7c31db90c3c
karenhenwood <karenhenwood@gmail.com>
2017-05-16 21:05:17 -0700

    user array passing between pages and new user creation complete

--- login/js/app.js
+++ login/js/app.js
@@ -1,42 +1,16 @@
-//Define ok users and passwords and organize into an array
-// var user1 = {
-// 	un: "un1",
-// 	pw: "pw1",
-// }
-
-// var user2 = {
-// 	un: "un2",
-// 	pw: "pw2",
-// }
-
-// var user3 = {
-// 	un: "un3",
-// 	pw: "pw3",
-// }
-
-//var userArray = [user1, user2, user3];
-// console.log(userArray)
-// console.log(user1)
-// console.log(userArray[0])
-// var json = JSON.stringify(userArray)
-// console.log(json)
-// var undo = JSON.parse(json)
-// console.log(undo)
-
-
-// var newuser={
-// 	un: "un4",
-// 	pw: "pw3",
-// }
-// undo.push(newuser)
-// console.log(undo)
+//Define an array to hold users as they're added in the new registration page
 var userArray = []
-window.onload = function(){
-	userArray.push(JSON.parse(sessionStorage.getItem("passingArray")))
- 	console.log(userArray)
-//  	console.log(JSON.stringify(userArray))
-//  	console.log(sessionStorage.getItem("passingArray"))
- }
+
+//On first load, skip this step (null check)
+//But when returning to the main page after adding users, get all the added users
+//out of the array passed between the pages and into the working userArray
+if(JSON.parse(sessionStorage.getItem('passingArray')) != null){
+	for(i=0;i<JSON.parse(sessionStorage.getItem('passingArray')).length;i++){
+	 	userArray.push(JSON.parse(sessionStorage.getItem('passingArray'))[i])
+	}
+}
+//log the list of users for convenience and troubleshooting
+console.log(userArray)
 
 //When the enter button is clicked, call the fucntion to grab the id and pw entered
 //and check user authentication
@@ -48,6 +22,7 @@ function authenticate(){
 	var un = document.getElementById('un').value;
 	var pw = document.getElementById('pw').value;
 
+//once users have been added to the user array,
 //check the stored usernames and passwords against the known
 //if there's a match let them in
 //if you get to the end of the array of known users
@@ -58,17 +33,25 @@ function authenticate(){
 //  Valid un and invalid pw -> clear pw only
 //Invalid un and   valid pw -> clear both fields
 //Invalid un and invalid pw -> clear both fields
-	for(i=0; i<userArray.length; i++){
-		if((un == userArray[i].un) && (pw == userArray[i].pw)){
-			alert("You're in!");
-			document.getElementById('un').value = "";
-			document.getElementById('pw').value = "";
-			break;
+	if(userArray.length>0){
+		for(i=0; i<userArray.length; i++){
+			if((un == userArray[i].un) && (pw == userArray[i].pw)){
+				alert("You're in!");
+				document.getElementById('un').value = "";
+				document.getElementById('pw').value = "";
+				break;
+			}
+			if(i==userArray.length-1 || userArray.length==0){
+				console.log('working')
+				troubleshoot(un, pw);
+			}
 		}
-		if(i==userArray.length-1){
-			troubleshoot(un, pw);
-		}
+	}else{//enter here on first load when there are no users in the array yet
+		alert("No match found. Please click the Create Account link to register a new username");
+		document.getElementById('un').value = "";
+		document.getElementById('pw').value = "";
 	}
+
 };
 //If there is a match in the known usernames clear only the pw field so user
 //doesn't have to retype un
@@ -80,7 +63,7 @@ function troubleshoot(un, pw){
 				document.getElementById('pw').value = "";
 				break;
 			}
-			if(j==userArray.length-1){
+			if(j==userArray.length-1 || userArray.length==0){
 				alert("No match found. Please click the Create Account link to register a new username");
 				document.getElementById('un').value = "";
 				document.getElementById('pw').value = "";
--- login/js/register.js
+++ login/js/register.js
@@ -1,38 +1,16 @@
-//Define ok users and passwords and organize into an array
-// var user1 = {
-// 	un: "un1",
-// 	pw: "pw1",
-// }
+//Define an array to hold users as they're added in the new registration page
+var userArray = []
 
-// var user2 = {
-// 	un: "un2",
-// 	pw: "pw2",
-// }
-
-// var user = {
-//  	un: "un",
-//  	pw: "pw",
-//  }
-
-// var array = [];
-// console.log(array)
-// var item = {
-// 	prop1: "1",
-// 	prop2: "2",
-// }
-// console.log(item)
-// array.push(item)
-// console.log(array)
-
-
-
- var userArray = [];
-// console.log(userArray)
-
-window.onload = function(){
-  	userArray.push(JSON.parse(sessionStorage.getItem("passingArray")))
-  	console.log(userArray)
+//On first load, skip this step (null check)
+//But when returning to the main page after adding users, get all the added users
+//out of the array passed between the pages and into the working userArray
+if(JSON.parse(sessionStorage.getItem('passingArray')) != null){
+	for(i=0;i<JSON.parse(sessionStorage.getItem('passingArray')).length;i++){
+	 	userArray.push(JSON.parse(sessionStorage.getItem('passingArray'))[i])
+	}
 }
+//log the list of users for convenience and troubleshooting
+console.log(userArray)
 
 document.getElementById('enternew').onclick = function(){verifyNew()};
 //Verify new user function
@@ -40,13 +18,14 @@ function verifyNew(){
 
 	var un = document.getElementById('unnew').value;
 
+    //once users have been added to the user array,
 	//check the new usernames against the known
 	//if there's a match kick them back
 	//if you get to the end of the array of known users
 	//i will have incremented all the way to the array's length
 	//this tells you there was no match and calls verifySecure
 	//to ensure their password is long enough
-	if(userArray[0]!=null){
+	if(userArray.length>0){
 		for(i=0; i<userArray.length; i++){
 			if(un == userArray[i].un){
 				alert("Username exists, please create a new username");
@@ -82,10 +61,10 @@ function addUser(){
 		un: document.getElementById('unnew').value,
 		pw: document.getElementById('pwnew').value,
 	}
-	userArray.push(newUser)
 
- 	console.log(userArray)
-	sessionStorage.setItem("passingArray", userArray)
+	//add the user to the array, put the array into the shared array, clear the inputs
+	userArray.push(newUser)
+	sessionStorage.setItem('passingArray', JSON.stringify(userArray));
 	document.getElementById('unnew').value = "";
 	document.getElementById('pwnew').value = "";
 
